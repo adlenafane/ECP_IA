f6997ee53441e5b3e57cf13e51448f63d430389f
adlenafane <adlen.afane@gmail.com>
2013-01-25 11:02:08 +0100

    tests file added

---
+++ tests.py
@@ -0,0 +1,10 @@
+from utility import *
+
+
+print '\n##### Utility distance tests #####\n'
+print "computeMinDistance((1,2),(1,4)):", computeMinDistance((1,2),(1,4))
+print "findNextMove((1,2),(1,4)):", findNextMove((1,2),(1,4))
+
+print "computeMinDistance((1,2),(3,4)):", computeMinDistance((1,2),(3,4))
+print "findNextMove((1,2),(3,4)):", findNextMove((1,2),(3,4))
+print '\n##########'
