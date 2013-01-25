91f93db79ad0af229e3a8a45df6b6fa32d6855b7
adlenafane <adlen.afane@gmail.com>
2013-01-25 11:15:14 +0100

    Added the failed/pass system

--- tests.py
+++ tests.py
@@ -1,10 +1,20 @@
 from utility import *
 
+def passFailedTest(function, target_value):
+	print "Result:", str(function)
+	print ("----Failed", "Pass")[function == target_value]
 
 print '\n##### Utility distance tests #####\n'
-print "computeMinDistance((1,2),(1,4)):", computeMinDistance((1,2),(1,4))
-print "findNextMove((1,2),(1,4)):", findNextMove((1,2),(1,4))
 
-print "computeMinDistance((1,2),(3,4)):", computeMinDistance((1,2),(3,4))
-print "findNextMove((1,2),(3,4)):", findNextMove((1,2),(3,4))
+print "computeMinDistance((1,2),(1,4)):"
+passFailedTest(computeMinDistance((1,2),(1,4)), 2)
+
+print "findNextMove((1,2),(1,4)):"
+passFailedTest(findNextMove((1,2),(1,4)), (1,3))
+
+print "computeMinDistance((1,2),(3,4)):"
+passFailedTest(computeMinDistance((1,2),(3,4)), 2)
+
+print "findNextMove((1,2),(3,4)):"
+passFailedTest(findNextMove((1,2),(3,4)),(2,3))
 print '\n##########'
