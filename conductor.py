c340790c15e8f324f2c58134f8f9aa511d6d2c09
adlenafane <adlen.afane@gmail.com>
2013-03-13 01:52:10 +0100

    Commented print

--- conductor.py
+++ conductor.py
@@ -5,7 +5,7 @@ class Conductor():
 	""" 
 		This class performs the computation of the tree and output the good move
 	"""
-	def __init__(self, our_ia = Stuxnet(), other_ia = Stuxnet()):
+	def __init__(self, our_ia = Stuxnet(config.nous, config.eux), other_ia = Stuxnet(config.eux, config.nous)):
 		self.our_ia = our_ia
 		self.other_ia = other_ia
 		self.best_order = []
@@ -17,16 +17,16 @@ class Conductor():
 			player = 1 if we are playing and -1 for the other
 		'''
 		current_board = current_move[0]
-		print "max_level", max_level
+		# print "max_level", max_level
 		if max_level == 0:
 			# Score, move, needed_depth
 			return (current_board.score(), None, 0)
 
-		print "current_move", current_move
-		print "current_board", current_board
+		# print "current_move", current_move
+		# print "current_board", current_board
 		current_score = current_board.score()
 
-		print 'current_score', current_score
+		# print 'current_score', current_score
 		if abs(current_score) == config.cst_heuri:
 			# Score, move, needed_depth
 			return current_score, None, 0
@@ -37,10 +37,10 @@ class Conductor():
 		# Get the next moves
 		if  player == 1:
 			next_moves = self.our_ia.find_smart_move()
-			print "next_moves", next_moves
+			# print "next_moves", next_moves
 		else:
 			next_moves = self.other_ia.find_smart_move()
-			print "next_moves", next_moves
+			# print "next_moves", next_moves
 
 		# Initialize the variables
 		best_score = -player*float('inf')
@@ -50,9 +50,9 @@ class Conductor():
 		# Let's dig!
 		for move in next_moves:
 			current_score, current_move, current_depth = self.minmax(-player, move, needed_depth)
-			print 'minmax-score', current_score
-			print 'minmax-move', current_move
-			print 'minmax-depth', current_depth
+			# print 'minmax-score', current_score
+			# print 'minmax-move', current_move
+			# print 'minmax-depth', current_depth
 			if max_level == 4:
 				print "Score for column %s: %s"%(current_move, current_score)
 			if (player==-1 and current_score<best_score) or (player==1 and current_score>best_score) or (current_score==best_score and current_depth<=needed_depth):
