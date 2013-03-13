563e5b7e9ab8119a044f10b7e31b81aa8dc90202
adlenafane <adlen.afane@gmail.com>
2013-03-13 14:06:02 +0100

    server config

---
+++ Old IA/Deprecated/config.py
@@ -0,0 +1,13 @@
+board = {}
+home = ()  #stock le tuple de coodonnees de notre maison. variable qui servira a identifier si on est des v ou des w
+nous = ''  #variable qui contiendra 'v' si on est des v ou 'w' si on est des w
+eux = ''  #variable qui contiendra 'w' si on est des v ou 'v' si on est des w
+nous_fixe = ''
+eux_fixe = ''
+Xsize = 0
+Ysize = 0
+cst_heuri = 0
+address = ("127.0.0.1",5555)
+nb_of_h_positions_at_start = 0
+dominance= 0   # variable needed to know wich sound to play at the end of the game
+best_move = []
